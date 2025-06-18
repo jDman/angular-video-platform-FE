97a6fb571f6de4acfbd71d497e117e3cdf34c442
jDman <davis.jaym@gmail.com>
2025-06-18 23:12:00 +0100

    Add karma config and first component with tests

---
+++ src/app/shared/components/button/button.spec.ts
@@ -0,0 +1,68 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Button } from './button';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+describe('Button', () => {
+  let component: Button;
+  let fixture: ComponentFixture<Button>;
+  let buttonTextSpan: HTMLSpanElement;
+  let buttonIconSpan: HTMLSpanElement;
+  let buttonDe: DebugElement;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [Button]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(Button);
+    buttonDe = fixture.debugElement.query(By.css('.button'));
+    buttonTextSpan = fixture.nativeElement.querySelector('.button__text');
+    buttonIconSpan = fixture.nativeElement.querySelector('.button__icon');
+    fixture.componentRef.setInput('buttonText', 'Some text');
+    component = fixture.componentInstance;
+    
+    fixture.detectChanges();
+  });
+
+  it('should create the button component with the button type', () => {
+    expect(component).toBeTruthy();
+    const button = fixture.nativeElement.querySelector('.button');
+
+    expect(button.type).toBe('button');
+  });
+
+  it('should update the button type', () => {
+    fixture.componentRef.setInput('buttonType', 'submit');
+    fixture.detectChanges();
+
+    const button = fixture.nativeElement.querySelector('.button');
+
+    expect(button.type).toBe('submit');
+  });
+
+  it('should render span with correct button text' , () => {
+    expect(buttonTextSpan.textContent).toContain(component.buttonText());
+  });
+
+  it ('should not render a span with the buttonIcon class when hasIcon set to false', () => {
+    expect(buttonIconSpan).toBeNull();
+  });
+
+  it ('should render a span with the buttonIcon class when hasIcon set to true', () => {
+    fixture.componentRef.setInput('hasIcon', true);
+    fixture.detectChanges();
+    buttonIconSpan = fixture.nativeElement.querySelector('.button__icon');
+    expect(buttonIconSpan).toBeTruthy();
+  });
+
+  it('should emit when clicked', () => {
+    const pointerEvent = new PointerEvent('pointerdown');
+    spyOn(component.buttonClick, 'emit').and.callFake(() => {});
+
+    buttonDe.triggerEventHandler('pointerdown', pointerEvent);
+
+    expect(component.buttonClick.emit).toHaveBeenCalledWith(pointerEvent);
+  });
+});
---
+++ src/app/shared/components/button/button.ts
@@ -0,0 +1,20 @@
+import { Component, input, output } from '@angular/core';
+
+type ButtonType = 'button' | 'reset' | 'submit';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.html',
+  styleUrl: './button.scss'
+})
+export class Button {
+  buttonType = input<ButtonType>('button');
+  buttonText = input.required<string>();
+  hasIcon = input<boolean>(false);
+  buttonClick = output<PointerEvent>();
+
+  handleButtonClick(event: PointerEvent) {
+    event.preventDefault();
+    this.buttonClick.emit(event);
+  }
+}
